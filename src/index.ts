0d68d7acc7713a035b02309964444292733fb9f2
Filippo Fonseca <filifonsecacagnazzo@gmail.com>
2021-08-20 18:00:06 +0000

    extract values

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 /**
  * An *incredibly* complex util function that returns the value of a random index (from a provided array).
  */
-export function random(data: any[]) {
-    const randomGen: number = Math.floor(Math.random() * data.length);
-    const randomIndex: any = data[randomGen];
 
-    return randomIndex;
+import { getRandomValue } from "./random";
+
+export function random(data: any[]) {
+    const run = getRandomValue(data);
+
+    return run;
 }
 
 
---
+++ src/random.ts
@@ -0,0 +1,44 @@
+export function getRandomValue(data: any[]) {
+    const randomGen: number = Math.floor(Math.random() * data.length);
+    const randomIndex: any = data[randomGen];
+
+    return randomIndex;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
