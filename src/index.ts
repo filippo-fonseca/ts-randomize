2f762f9b2704a13f870ce7c54e8b34788990a4d7
Brian3647 <victoris3647@gmail.com>
2021-09-20 19:49:49 +0200

    Fixed workflow, added code docs + generics and removed unused files/dependencies.

--- src/index.ts
+++ src/index.ts
@@ -4,46 +4,13 @@
 
 import { getRandomValue } from "./random";
 
-export function random(data: any[]) {
-    const run = getRandomValue(data);
+/**
+ * Returns a random item of an array
+ * @param data The array
+ * @returns A random item of the given array
+ */
+export function random<T>(data: T[]): T {
+    const run = getRandomValue<T>(data);
 
     return run;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
